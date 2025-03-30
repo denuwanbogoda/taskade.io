fd55b17df0fa3efa42b974196d94d8d9998b847c
denuwanbogoda <binaradenuwanguitar@gmail.com>
2025-03-30 19:17:35 +0530

    Update page.tsx

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,46 +1,30 @@
-import { redirect } from "next/navigation"; // `next/navigation` comes first
-import { useEffect, useState } from "react"; // `react` is second
-import { getProviders, useSession } from "next-auth/react"; // Import only useSession from next-auth/react
+import { redirect } from "next/navigation";
+import { auth, getProviders } from "@/auth";
 import { DASHBOARD_URL } from "@/constants";
 import { DemoLogin } from "./DemoLogin";
+import { NextAuthLogin } from "./NextAuthLogin";
+import styles from "./signin.module.css";
 
-const SignInPage = () => {
-  const [providers, setProviders] = useState<Record<string, string> | null>(null);
-  const { data: session, status } = useSession();
+export default async function SignInPage() {
+  const session = await auth();
 
-  useEffect(() => {
-    const getAuthProviders = async () => {
-      const providers = await getProviders();
-      if (providers) {
-        setProviders(providers); // Set providers only if they are not null
-      }
-    };
-    getAuthProviders();
-  }, []);
-
-  // If session exists, redirect to the dashboard page
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
-
+  // If logged in, go to dashboard
   if (session) {
     redirect(DASHBOARD_URL);
   }
 
+  const providers = await getProviders();
   return (
-    <div>
-      <h1>Sign In</h1>
-      <div>
-        {providers ? (
-          Object.values(providers).map((provider) => (
-            <DemoLogin key={provider.name} provider={provider} />
-          ))
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2 className={styles.title}>Sign in to your account</h2>
+        {providers && providers.credentials ? (
+          <DemoLogin />
         ) : (
-          <div>No providers available</div>
+          <NextAuthLogin providers={providers} />
         )}
-      </div>
+      </main>
+      <aside className={styles.aside} />
     </div>
   );
-};
-
-export default SignInPage;
+}
